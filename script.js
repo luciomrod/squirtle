d314d7462f44837ab056c66694cbfb18db780664
Lucio <luciomedinawork@gmail.com>
2024-07-02 20:11:40 -0300

    page finished

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const message = document.getElementById('message');
+    const squirtleSound = document.getElementById('squirtleSound');
     let isSquirtle = true;
 
     setInterval(() => {
         message.textContent = isSquirtle ? 'Squirtle' : '';
+        if (isSquirtle) {
+            squirtleSound.currentTime = 0;
+            squirtleSound.play();
+        }
         isSquirtle = !isSquirtle;
     }, 1000);
 });
